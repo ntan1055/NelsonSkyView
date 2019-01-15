fe1ae8c2fcdb7f38c25079fa63e01c749704af0e
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2019-01-15 08:45:59 -0500

    Update SkyView.java

--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -9,13 +9,32 @@ public class SkyView
 
     public SkyView(int numRows, int numCols, double[] scanned)
     {
-        
+        view = new double[numRows][numCols];
+        int i = 0;
+        for(int row = 0; row < numRows; row++)
+        {
+            if(row % 2 == 0)
+            {
+                for(int col = 0; col < numCols; col++)
+                {
+                    view[row][col] = scanned[i];
+                    i++;
+                }
+            }
+            else{
+                for(int col = numCols - 1; col >= 0; col--)
+                {
+                    view[row][col] = scanned[i];
+                    i++;
+                }
+            }
+        }
     }
 
     public double getAverage(int startRow, int endRow, int startCol, int endCol)
     {
         double average;
-        double sum = 0;
+        double sum = 0.0;
         int count = 0;
         for(int i = startRow; i < endRow; i++)
         {
