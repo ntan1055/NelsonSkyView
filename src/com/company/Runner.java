832d42c4d9b956a2fb3c5fde9f8b3d851596f384
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2019-01-16 08:37:31 -0500

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -1,9 +1,9 @@
 package com.company;
 
-public class Runner {
-
-    public static void main(String[] args) {
-	// write your code here
+public class Runner
+{
+    public static void main(String[] args)
+    {
         double[] scan = {0.3, 0.7, 0.8, 0.4, 1.4, 1.1, 0.2, 0.5, 0.2, 1.6, 0.6, 0.9};
         double[] scan2 = {0.3, 0.7, 0.8, 0.4, 1.4, 1.1};
 
